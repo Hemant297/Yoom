31adc41eb0e0980c87ab2f7e6050b9a37ea6c551
Hemant297 <hemant.khemka66@gmail.com>
2024-06-21 16:18:12 +0530

    initial commit upto sidebar

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    imgUrl: "/icons/home.svg",
+    route: "/",
+  },
+  {
+    label: "Upcoming",
+    imgUrl: "/icons/upcoming.svg",
+    route: "/upcoming",
+  },
+  {
+    label: "Previous",
+    imgUrl: "/icons/previous.svg",
+    route: "/previous",
+  },
+  {
+    label: "Recordings",
+    imgUrl: "/icons/recordings.svg",
+    route: "/recordings",
+  },
+  {
+    label: "Personal Room",
+    imgUrl: "/icons/personal_room.svg",
+    route: "/personal-room",
+  },
+];
