489f9ea5ce5ae98b3a7e1427b31da3bc80c1d37f
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-07-01 15:44:53 +0530

    Update layout.tsx

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -2,6 +2,17 @@ import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import React, { ReactNode } from "react";
 
+
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video Calling App",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+
 function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <main className="relative">
