11c2f5db249d7c8c48c88cff66ba8fe58f6add46
Hemant297 <hemant.khemka66@gmail.com>
2024-07-10 01:21:42 +0530

    Schedule meeting completed

--- components/EndCallButton.tsx
+++ components/EndCallButton.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { useCall, useCallStateHooks } from '@stream-io/video-react-sdk';
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
 
-import { Button } from './ui/button';
-import { useRouter } from 'next/navigation';
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 const EndCallButton = () => {
   const call = useCall();
@@ -11,23 +11,22 @@ const EndCallButton = () => {
 
   if (!call)
     throw new Error(
-      'useStreamCall must be used within a StreamCall component.',
+      "useStreamCall must be used within a StreamCall component."
     );
 
-  
   const { useLocalParticipant } = useCallStateHooks();
   const localParticipant = useLocalParticipant();
 
   const isMeetingOwner =
     localParticipant &&
-    call.state.createdBy &&
+    call?.state.createdBy &&
     localParticipant.userId === call.state.createdBy.id;
 
   if (!isMeetingOwner) return null;
 
   const endCall = async () => {
     await call.endCall();
-    router.push('/');
+    router.push("/");
   };
 
   return (
